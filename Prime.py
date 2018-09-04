91fea203b95323575427fb436969f72763103e84
Denis Belov <den-belov@yandex.ru>
2018-09-04 21:39:15 +0300

    Добавление генератора простых чисел

--- Prime.py
+++ Prime.py
@@ -1,62 +1,105 @@
-import random, math
+import random
+import math
 
 
-class Primes():
-    # НОД чисел a и b
-    def gcd(self, a, b):
-        if b == 0:
-            return a
-        return self.gcd(b, a % b)
+def gcd(a, b):
+    """НОД чисел a и b"""
+    if b == 0:
+        return a
+    return gcd(b, a % b)
 
-    # Умножение a на b по модулю m
-    def mul(self, a, b, m):
-        if b == 1:
-            return a
-        if b % 2 == 0:
-            t = self.mul(a, b/2, m)
-            return 2*t % m
-        return (self.mul(a, b-1,m) + a) % m
 
-    # Возведение a в степень b по модулю m
-    def pows(self, a, b, m):
-        if b == 0:
-            return 1;
-        if b % 2 == 0:
-            t = self.pows(a, b/2, m)
-            return self.mul(t, t, m) % m
-        return self.mul(self.pows(a, b-1, m), a, m) % m
+def multiply(a, b, m):
+    """"Умножение a на b по модулю m"""
+    if b == 1:
+        return a
+    if b % 2 == 0:
+        t = multiply(a, b/2, m)
+        return 2*t % m
+    return (multiply(a, b-1, m) + a) % m
 
-    # Проверка на простоту тестом Ферма
-    # https://ru.wikipedia.org/wiki/Тест_Ферма
-    # По тестам работает медленнее, чем перебор
-    def ferma(self, x):
-        random.seed
-        if x == 2:
-            return True
-        for _ in range(100):
-            a = (random.randint(0, 32767) % (x - 2)) + 2
-            if self.gcd(a,x) != 1:
-                return False
-            if self.pows(a, x-1, x) != 1:
-                return False
+
+def powers(a, b, m):
+    """"Возведение a в степень b по модулю m"""
+    if b == 0:
+        return 1
+    if b % 2 == 0:
+        t = powers(a, b/2, m)
+        return multiply(t, t, m) % m
+    return multiply(powers(a, b-1, m), a, m) % m
+
+
+def ferma(n, cnt=100):
+    """"Проверка на простоту тестом Ферма
+        https://ru.wikipedia.org/wiki/Тест_Ферма
+        Работает медленнее, чем перебор"""
+    return False
+    if n == 2:
         return True
+    if n < 2:
+        return False
+    random.seed
+    for _ in range(cnt):
+        a = (random.randint(0, 2 ** 8) % (n - 2)) + 2
+        if gcd(a, n) != 1 or powers(a, n-1, n) != 1:
+            return False
+    return True
 
-    # Проверка на простоту перебором
-    def brute(self, n):
-        for i in range(2, int(n ** .5)):
-            if n%i == 0:
-                return False
-            return True
 
-    # Проверка на простоту по теореме Вильсона
-    def wilson(self, n):
-        return not (math.factorial(n - 1) + 1) % n
+def brute(n):
+    """Проверка на простоту перебором"""
+    if n < 2:
+        return False
+    for iCounter in range(2, int(n ** .5) + 1):
+        if n % iCounter == 0:
+            return False
+    return True
+
+
+def wilson(n):
+    """Проверка на простоту по теореме Вилсона
+        Работает медленно из-за необходимости вычиления факториала
+        https://ru.wikipedia.org/wiki/Теорема_Вильсона"""
+    if n < 2:
+        return False
+    return not (math.factorial(n - 1) + 1) % n
+
+
+def wilson_generator(num_count, start=2, finish=0):
+    """Генератор простых чисел, с помощью теоремы Вилсона
+        num_count - количество сгенерированных простых чисел
+        start - если указан этот параметр, простые числа будут больше этого числа
+        finish - если указан этот параметр, простые числа будут не больше этого числа
+        (даже если в итоге получилось меньше, чем num_count)
+        """
+    count = 0
+    if start < 2:
+        start = 2
+    n = start
+    fact = math.factorial(start - 1)
+    while count < num_count:
+        if finish != 0 and n > finish:
+            break
+        if not (fact + 1) % n:
+            count += 1
+            yield n
+        fact *= n
+        n += 1
 
 
 if __name__ == '__main__':
     from Profiler import Profiler
-    pr = Primes()
-    pflr = Profiler()
-    pflr.start()
-    print(pr.brute(14353453545434071234353453454723131124327))
-    pflr.finish()
+    prflr = Profiler()
+    """"
+    # Пример проверки на простоту
+    prflr.start()
+    for num in range(1, 100):
+        print(brute(num), ferma(num), wilson(num))        
+    prflr.finish()
+    """
+    """
+    # Пример генерации простых чисел
+    prflr.start()
+    print([i for i in wilson_generator(100)])
+    prflr.finish()
+    """
--- Profiler.py
+++ Profiler.py
@@ -1,16 +1,20 @@
+"""
+Простой профайлер для измерения времени работы куска кода
+"""
 import time
 
 
-# Измерение время работы куска кода
-class Profiler():
+
+class Profiler:
     def __init__(self):
         self._startTime = 0
 
     def start(self):
+        """"Старт измерения"""
         self._startTime = time.time()
 
-    def finish(self, rtrn = False):
-        s = "Elapsed time: {:.3f} sec".format(time.time() - self._startTime)
+    def finish(self, rtrn=False):
+        """Конец измерения"""
         if rtrn:
             return format(time.time() - self._startTime)
         else:
